8640a6152ed189e16f930261c11717f7f254a5d5
EnjoyYourDay <72964028+EnjoyYourDay@users.noreply.github.com>
2020-10-28 11:53:40 +0100

    28oct2020 (removed action bar on top)

--- app/src/main/java/com/example/t_mobop_werewolf/PlayingActivity.kt
+++ app/src/main/java/com/example/t_mobop_werewolf/PlayingActivity.kt
@@ -2,11 +2,16 @@ package com.example.t_mobop_werewolf
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
 
 class PlayingActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_playing)
+
+        val toolbar = findViewById(R.id.play_actionbar)
+        setSupportActionBar(toolbar)
     }
 }
 
