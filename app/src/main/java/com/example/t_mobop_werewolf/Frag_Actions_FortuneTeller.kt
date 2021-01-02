30cf1b803175590ae3dfed0ac20dd2f58e764655
anuraag.pothula <apothula95@gmail.com>
2021-01-02 18:02:01 +0100

    Fortune Teller modifications

--- app/src/main/java/com/example/t_mobop_werewolf/Frag_Actions_FortuneTeller.kt
+++ app/src/main/java/com/example/t_mobop_werewolf/Frag_Actions_FortuneTeller.kt
@@ -1,18 +1,27 @@
 package com.example.t_mobop_werewolf
 
 import android.os.Bundle
+import android.text.Layout
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.RadioGroup
 import androidx.fragment.app.Fragment
+import com.example.t_mobop_werewolf.FirebaseData.GeneralDataModel
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_playing.*
+import kotlinx.android.synthetic.main.fragment_waiting_room.*
 
 class Frag_Actions_FortuneTeller : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
+    // onCreateView is similar to onCreate for an activity
+    // be careful not to refer to an UI element as they are not yet available (return null)
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -31,18 +40,30 @@ class Frag_Actions_FortuneTeller : Fragment() {
 
 
         buttonReveal.setOnClickListener {
-            Log.d("MainActivity", "1 role revealed")
-            // get the player pseudo
-            // display it
-            // change the flag for next turn
+            val roomName = GeneralDataModel.localRoomName
+            val pseudo = GeneralDataModel.localPseudo
+            Log.d("MainActivity", "1 role reavealed")
+            playersRadioGroup.setOnCheckedChangeListener { group, checkedId ->// get the player pseudo
+                val playerPseudo = GeneralDataModel.getAnyData("$roomName/Players/Player$checkedId/Pseudo") as String// display it
+                val playerRole = GeneralDataModel.getAnyData("$roomName/Players/Player$checkedId/Role") as String // display it
 
+                val roleText = textView
+                roleText.layoutParams= LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT)
+                roleText.setPadding(24,0,0,16)
+                roleText.setText(playerPseudo + "is a" + playerRole)
+
+                //view.findViewById<Layout>(R.id.tellerList)?.addView(roleText)
+                //TODO change the flag for next turn
+            }
         }
 
         // add a button that waits on the player to have
 
         buttonPass.setOnClickListener {
             Log.d("MainActivity", "FortuneTeller has pass her turn")
-            // change the flag for next turn
+            //TODO change the flag for next turn
         }
     }
 }
