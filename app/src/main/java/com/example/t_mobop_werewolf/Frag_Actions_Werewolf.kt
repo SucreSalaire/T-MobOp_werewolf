3ddb14e0e61901aa790e78f9147e99b28413fe50
anuraag.pothula <apothula95@gmail.com>
2021-01-02 09:03:24 +0100

    minor modif to frag_activity_werewolf

--- app/src/main/java/com/example/t_mobop_werewolf/Frag_Actions_Werewolf.kt
+++ app/src/main/java/com/example/t_mobop_werewolf/Frag_Actions_Werewolf.kt
@@ -46,16 +46,35 @@ class Frag_Actions_Werewolf : Fragment() {
 
             playersRadioGroup.setOnCheckedChangeListener { group, checkedId ->
                 if (checkedId != null) {
-                    //TODO Verify that target isn't a werewolf (make checkboxes into radio btn)
+
                     val pathRole = "$roomName/Players/$checkedId/--" //TODO got to find path to role
                     val selectedRole = GeneralDataModel.getAnyData(pathRole) as String
+                    val pathSelected = "$roomName/Players/$checkedId/--" //TODO create a path for selected targets
+                    val pathListSelected = "$roomName/Players/$checkedId/--" //TODO create a path for list of selected targets
+
+                    // Verify that target isn't a werewolf
                     if (selectedRole == "Werewolf") {
                         Toast.makeText(context,"Select a villager, not an other werewolf",Toast.LENGTH_LONG).show()
                     }
+                    else {
+                        var votes: Int = GeneralDataModel.getAnyData(pathSelected) as Int
+                        GeneralDataModel.setAnyData(pathSelected, votes+1)
+                    }
 
                     //TODO Check if a majority of werewolves have selected a target
+                    val voted = arrayListOf<Int>(GeneralDataModel.getAnyData(pathListSelected) as Int)
+                    val max: Int? = voted.max()
+                    var nMax: Int = 0
+                    for (k in voted){
+                        if(k == max) nMax = nMax + 1
+                    }
+                    if(nMax > 1) Toast.makeText(context,"Reselect a target, we had a tie",Toast.LENGTH_LONG).show()
+                    else{ //kill selected player
+                        val path2bkilled = "$roomName/Players/$checkedId/--" //TODO create a path for target to be killed
+                        val tobkilled: String = GeneralDataModel.getAnyData(path2bkilled) as String
+                        GeneralDataModel.killPlayer(tobkilled)
+                    }
 
-                    //TODO kill selected player
                 }
                 Toast.makeText(context,"Select a villager to kill and try again",Toast.LENGTH_LONG).show()
             }
